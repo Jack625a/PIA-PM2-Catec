f33ae79518f000c14616175e974723c4b6025805
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-02-29 11:00:07 -0400

    Ejercicio Servidro Completo

--- node/ejercicioServidor/servidor.js
+++ node/ejercicioServidor/servidor.js
@@ -79,11 +79,20 @@ const servidor=http.createServer((req,res)=>{
                     resultado='Operacion no valida';
 
             }
-        })
+        res.writeHead(200,{
+            'Content-Type':'text/plain'
+        });
+        res.end(resultado.toString());
+        });
+    }else{
+        res.writeHead(404,{
+            'Content-Type':'text/html'
+        });
+        res.end('Error 404, archivo no existente!');
+
     }
-})
+});
 
-
-
-
-
+servidor.listen(puerto,host,()=>{
+    console.log(`Servidor Activo: http://${host}:${puerto}`);
+});
