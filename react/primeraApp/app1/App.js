e450b181be158972f035f6a5e34a3bf51a3f2d88
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-03-12 12:14:17 -0400

    React expo

---
+++ react/primeraApp/app1/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo} >Primera Aplicacion catec</Text>
+      <Text style={styles.titulo}>Hola</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo:{
+    fontSize:30,
+    color: 'blue',
+    fontWeight:'bold',
+  } 
+});
